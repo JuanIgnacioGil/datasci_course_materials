0e450054e48f52cc36884a5d2f8d3233ed8afcc7
Juan Ignacio Gil Gómez <iwan.manjak@gmail.com>
2014-07-05 14:09:38 +0200

    Resultados del primer problema Creamos diccionario con los sentiment scores

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,30 @@
 import sys
 
-def hw():
-    print 'Hello, world!'
 
-def lines(fp):
-    print str(len(fp.readlines()))
+###############################################################################
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    #Creamos un diccionario
+    diccionario=crear_diccionario(sent_file)
+
+
+###################################################################################
+
+def crear_diccionario(afinnfile):
+    
+    scores = {} # initialize an empty dictionary
+
+    for line in afinnfile:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+
+    return scores
+
+
+###################################################################################
 
 if __name__ == '__main__':
     main()
