1d8945f08e8b3c8f7dfc199ff73b17b4c91ce3c6
Juan Ignacio Gil Gómez <iwan.manjak@gmail.com>
2014-07-09 21:24:06 +0200

    Problema 3 resuelto

--- assignment2/union.sql
+++ assignment2/union.sql
@@ -6,7 +6,7 @@
 
 SELECT COUNT(*) FROM
 (
- select * from frequency where docid="10398_txt_earn" and count=1
+ select term from frequency where docid="10398_txt_earn" and count=1
  UNION
- select * from frequency where docid="925_txt_trade" and count=1
+ select term from frequency where docid="925_txt_trade" and count=1
 );
