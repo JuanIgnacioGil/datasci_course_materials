0fd7872110de02045fc5f76844ced34c3f5d4558
Juan Ignacio Gil Gómez <iwan.manjak@gmail.com>
2014-07-12 19:33:57 +0200

    Problema similirity matrix resuelto

--- assignment2/pruebas.sql
+++ assignment2/pruebas.sql
@@ -1,1 +1,4 @@
-SELECT COUNT(term) FROM (SELECT term FROM frequency GROUP BY term);
+select sum(d1.count*d2.count)
+from (select term,count from frequency where docid='10080_txt_crude') d1
+left join (select term, count from frequency where docid='17035_txt_earn') d2
+on d1.term=d2.term;
