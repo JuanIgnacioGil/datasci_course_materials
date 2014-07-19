63cae4abff51668b716b4750ea4ca1ada2e81090
Juan Ignacio Gil Gómez <iwan.manjak@gmail.com>
2014-07-19 18:01:21 +0200

    Solucionado problema 3.4 (assimetric_frienships)

---
+++ assignment3/asymmetric_friendships.py
@@ -0,0 +1,42 @@
+import MapReduce
+import sys
+
+"""
+    Problem 4
+    The relationship "friend" is often symmetric, meaning that if I am your friend, you are my friend. Implement a MapReduce algorithm to check whether this property holds. Generate a list of all non-symmetric friend relationships.
+    
+    Map Input
+    Each input record is a 2 element list [personA, personB] where personA is a string representing the name of a person and personB is a string representing the name of one of personA's friends. Note that it may or may not be the case that the personA is a friend of personB.
+    
+    Reduce Output
+    The output should be the full symmetric relation. For every pair (person, friend), you will emit BOTH (person, friend) AND (friend, person). However, be aware that (friend, person) may already appear in the dataset, so you may produce duplicates if you are not careful.
+    
+    You can test your solution to this problem using friends.json:
+    
+    $ python asymmetric_friendships.py friends.json
+    You can verify your solution by comparing your result with the file asymmetric_friendships.json.
+    
+"""
+
+mr = MapReduce.MapReduce()
+
+
+
+def mapper(record):
+    ordenado=sorted(record)
+    mr.emit_intermediate((ordenado[0],ordenado[1]),record)
+
+
+def reducer(key,record):
+    
+    if len(record)==1:
+        mr.emit((key[0],key[1]))
+        mr.emit((key[1],key[0]))
+
+
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
